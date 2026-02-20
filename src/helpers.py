610ee8ea82e854ad5dfe7ee37a650a5361f42ae5
simonvaser <patrosidhu80@gmail.com>
2026-02-20 14:27:34 +0530

    Refactor: improve src/helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -1069,3 +1069,14 @@ def cache_result(func):
             _cache[args] = func(*args)
         return _cache[args]
     return wrapper
+
+# Performance optimization
+
+def cache_result(func):
+    """Simple memoization decorator."""
+    _cache = {}
+    def wrapper(*args):
+        if args not in _cache:
+            _cache[args] = func(*args)
+        return _cache[args]
+    return wrapper
