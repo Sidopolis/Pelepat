6bb66ec37e93460f6379a59552f553a62e07c2a3
simonvaser <patrosidhu80@gmail.com>
2026-02-24 16:34:43 +0530

    Refactor: improve src/helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -1480,3 +1480,13 @@ def setup_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     return logger
+
+# Logging improvements
+
+import logging
+
+def setup_logger(name):
+    """Create a configured logger instance."""
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    return logger
